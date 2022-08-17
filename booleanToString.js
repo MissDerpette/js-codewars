355cd1f33b3be5e8e35ed8717a66af0a4ee14bfc
missderpette <mderpette@gmail.com>
2022-08-17 18:00:10 +0800

    boolean to string

---
+++ booleanToString.js
@@ -0,0 +1,7 @@
+function booleanToString(b){
+    return b.toString(); 
+ }
+
+//  Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
