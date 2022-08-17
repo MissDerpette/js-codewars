324eb1cc6dd4a4bd9a582f6982f79b087de12f41
missderpette <mderpette@gmail.com>
2022-08-17 15:52:04 +0800

    remove string spaces and join the resultant

---
+++ removeStringSpaces.js
@@ -0,0 +1,10 @@
+function noSpace(x){
+    let noSpace = x.split(" ").join("")
+  
+     console.log(x)
+    
+    return noSpace;
+  }
+
+  //Simple, remove the spaces from the string, then return the resultant string.
+
