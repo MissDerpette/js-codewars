37e9db3063990399be766eae4252253b77a74ccb
missderpette <mderpette@gmail.com>
2022-08-17 15:29:19 +0800

    string repeat

---
+++ string-repeat.js
@@ -0,0 +1,9 @@
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+
+//   DESCRIPTION:
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+
