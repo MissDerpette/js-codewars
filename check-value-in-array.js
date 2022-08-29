c26f4bdb66c7959ab5244928adc5b87e5c31d2da
missderpette <mderpette@gmail.com>
2022-08-29 18:15:29 +0800

    how to check value IN array

---
+++ check-value-in-array.js
@@ -0,0 +1,15 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.å
+
+function check(a, x) {
+    console.log("a",a);
+    console.log("x", x);
+    return index = a.includes(x);
+   }
+
+// How to Debug
+// 1 check value of param 
+// 2 check which data type
