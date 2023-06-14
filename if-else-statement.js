4c71206c86128c92b82e742ab2645d0a968555e0
Kirstine Pineda <105693491+MissDerpette@users.noreply.github.com>
2023-06-14 12:11:41 +0800

    Create if-else-statement.js
    converted boolean values to string

---
+++ if-else-statement.js
@@ -0,0 +1,10 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  let word = "Yes"; 
+  let anotherWord = "No";
+ if ( bool === true) 
+   return word
+  else 
+    return anotherWord
+}
