3990a502efed54611e38b323ef4bbd8fd760b00f
missderpette <mderpette@gmail.com>
2023-11-06 23:48:24 +0800

    added and solved a problem

--- map-lesson-1.js
+++ map-lesson-1.js
@@ -12,17 +12,23 @@ function capitalized(text){
 let allCaps = family.map(capitalized)
 console.log(allCaps)
 // capitalized(family);
+
 //Arrow Function
 let allCaps2 = family.map(text => {
     return text.toUpperCase();
 });
 console.log(allCaps2)
-// **************************************************
-// name + ayala
-//Declared Function
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+// // name + ayala
+// //Declared Function
 const lastName = "Ayala"
+
 function addLastName(text){
- return text + " "+ lastName;
+ return text + " " + lastName;
 }
 let newName = family.map(addLastName);
 console.log(newName)
@@ -32,24 +38,33 @@ let newName2 = family.map(text => {
     return text + ' ' + lastName;
 })
 console.log(newName2)
-// **************************************************
-//name@gmail.com
-// name@gmail.com,name@gmail.com,name@gmail.com,name@gmail.com
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+// //name@gmail.com
+// // name@gmail.com,name@gmail.com,name@gmail.com,name@gmail.com
+
 //Declared Function
 let domain = "@gmail.com"
 function wDomain(text){
     return text + lastName + domain;
 }
-let emailAdd = family.map(wDomain).toString();
-console.log(emailAdd)
+let emailAdd = family.map(wDomain).toString().toLowerCase();
+console.log(emailAdd, 'here')
 
 //Arrow Function
 let wDomain2 = family.map(text => {
     return text + lastName + domain;
-}).toString();
+}).toString().toLowerCase();
 console.log(wDomain2)
-// **************************************************
-// replace chars with *** 
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+// // replace chars with *** 
 let passwords = [
     "iloveyou",
     "qwerty",
@@ -57,25 +72,33 @@ let passwords = [
     "admin",
     "adminadmin",
 ]
-//Declared Function
+// //Declared Function
 function maskPassword(text){
     return '*'.repeat(text.length);
 }
+
 let maskedPasswords = passwords.map(maskPassword);
 console.log(maskedPasswords)
-//Arrow Function
+
+// //Arrow Function
 let maskedPass = passwords.map(text => {
     return '*'.repeat(text.length);
 })
 console.log(maskedPass)
-// **************************************************
-// Mask everything * except last digit
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+// // Mask everything * except last digit
 let numbers = [
     "09161231234",
     "09109992233",
     "09194206969",
     "091842002420"
 ]
+
+//Declared Function
 function maskExceptLastDigit(text){
     let lastDigit = text.slice(-1);
     let maskedText = '*'.repeat(text.length - 1);
@@ -83,24 +106,30 @@ function maskExceptLastDigit(text){
 }
 let maskedNum = numbers.map(maskExceptLastDigit);
 console.log(maskedNum)
-//Arrow Function
+
+// //Arrow Function
 let maskedNumbers = numbers.map(text => {
     let lastNum = text.slice(-4);
     let maskedAlphabet = '*'.repeat(text.length - 4);
     return maskedAlphabet + lastNum;
 })
 console.log(maskedNumbers)
-// **************************************************
-// Exchange Rate
-// 1 USD = 50 PHP
-// Transform the data to object
-// {
-// Php: 50
-// USD :1
-// }
-// Print
-// ₱ 50.00
-// $ 1.00
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+// // Exchange Rate
+// // 1 USD = 50 PHP
+// // Transform the data to object
+// // {
+// // Php: 50
+// // USD :1
+// // }
+// // Print
+// // ₱ 50.00
+// // $ 1.00
+
 let amounts = [
     50.00,
     1000.00,
@@ -128,11 +157,27 @@ let amounts = [
 };
 let convertedRates = amounts.map(getConvertedRates);
 console.log(convertedRates);
-// **************************************************
-// TOO HARD
-// ASAP
-// // BRB
-// // first char, capitilize, then combine
+
+//Arrow Function
+let conversionRate = amounts.map(text => {
+    const exchangeRateUSD2 = getExchangeRates().Php; // Accessing the USD exchange rate
+    const equivalentInPhp2 = text / exchangeRateUSD2;
+    return { 
+        Php: `₱${text}`,
+        USD: `$${equivalentInPhp2}`
+    };
+})
+console.log(conversionRate)
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+// // TOO HARD
+// // ASAP
+// // // BRB
+// // // first char, capitilize, then combine
+
 let abbrv = [
     "as soon as possible",
     "be right back",
@@ -140,26 +185,32 @@ let abbrv = [
     "to be announce",
     "laugh out loud"
 ]
-//Declared Function
+// //Declared Function
 function getAbbr(text){
     let changeDataType = text.split(' ');
-    return changeDataType.map(char => char[0]).join("").toUpperCase();
+    let newDataAbbr = changeDataType.map(char => char[0]);
+    let toMixThemIn = newDataAbbr.join("").toUpperCase();
+    return toMixThemIn
 }
 let t = getAbbr(abbrv[0]);
 console.log(t) //for single array
 let finalArr = abbrv.map(getAbbr);
 console.log(finalArr) //for the whole array
 
-//Arrow Function
+// //Arrow Function
 let initials = abbrv.map(text => {
     let convertDataType = text.split(' ');
     return convertDataType.map(char => char[0]).join("").toUpperCase();
 })
 console.log(initials)
-// **************************************************
-// 1st task
-// 0916-123-1234
-//Declared Function
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+// // 1st task
+// // 0916-123-1234
+// //Declared Function
 let mobileNum = [
     "09161231234",
     "09109992233",
@@ -171,9 +222,6 @@ function splitMobileNum(text){
     let first = newArrNum.slice(0,4).join("")
     let mid  = newArrNum.slice(4,7).join("")
     let end = newArrNum.slice(-4).join("")
-    // let firstArr = first.join("");
-    // let midArr = mid.join("");
-    // let endArr = end.join("");
     let finalArr = first + ("-") + mid + ("-") + end;
     return finalArr;
 }
@@ -181,32 +229,41 @@ let s = splitMobileNum(mobileNum[0])
 console.log(s) // for the single array
 let newSetofArray = mobileNum.map(splitMobileNum);
 console.log(newSetofArray) // for the whole set
-//Arrow Function
+
+// //Arrow Function
 let newSetofArr = mobileNum.map(text => {
     let setTwoOfArr = text.split('');
     let firstArr = setTwoOfArr.slice(0,4).join("")
     let midArr  = setTwoOfArr.slice(4,7).join("")
     let endArr = setTwoOfArr.slice(-4).join("")
-    // let firstArr = first.join("");
-    // let midArr = mid.join("");
-    // let endArr = end.join("");
     let finalSetOfArr = firstArr + ("-") + midArr + ("-") + endArr;
     return finalSetOfArr;
 })
 console.log(newSetofArr)
-// 2nd task
-// +63916-123-1234
 
+
+// // 2nd task
+// // +63916-123-1234
+//Declared Function
 function replaceChar(text){
    return firstChar = text.replace("0", "+63");
 }
+
 let test = replaceChar(mobileNum[0]);
 console.log(test)
 let test2 = mobileNum.map(replaceChar);
 console.log(test2)
-let ansToTasks = newSetofArray.map(replaceChar).toString();
+let ansToTasks = newSetofArray.map(replaceChar);
 console.log(ansToTasks);
-// **************************************************
+
+let toReplaceChar = mobileNum.map(text => {
+    return firstChar = text.replace("0", "+63");
+})
+console.log(toReplaceChar, 'arrow')
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
 const normalWords = [
     "Hello World",
     "Love Maria",
@@ -214,62 +271,208 @@ const normalWords = [
     "Fedora Linux Other Word",
     "So much Word There Blah blAh blah"
 ]
-// camelCase
-// snake_case
-// kebab-case 
-// print like this 
-// camelCase: helloWorld
-// snake_case: hello_world
-// kebab-case: hello-world
+// // camelCase
+// // snake_case
+// // kebab-case 
+// // print like this 
+// // camelCase: helloWorld
+// // snake_case: hello_world
+// // kebab-case: hello-world
 
-//Declared Function camelCase
-function cCase(text){
-    return text.replace(/\s(.)/g, function(match, group1) {
+// Declared Function
+
+function toCamelCase(text) {
+     return text.replace(/\s(.)/g, function(match, group1) {
         return group1.toUpperCase();
-    });
-}
-let cCaseText = normalWords.map(text => cCase(text));
+    }); 
+} //to get the white spaces
+let cCaseHere = toCamelCase(normalWords[0]);
+console.log(cCaseHere)
+
+let cCaseText = normalWords.map(text => toCamelCase(text));
 console.log(cCaseText, "first char capitalized")
-
-function camelCaseText(text){
-   return newWord = text.toLowerCase();
-}
-
-//Arrow Function camelCase
+//first char to capitalized
 let finalLastLast = cCaseText.map(word =>{
     let lowerChar = word[0].toLowerCase();
     return  lowerChar + word.slice(1);
-});
+}); // combined code from above
 console.log(finalLastLast, "working as expected")
 
-//Declared Function snake_case
+
+// //Declared Function snake_case
 function toSnakeCase(text){
-    // return letters = text.toLowerCase().replace(/\s/g, '_'); REGEX
-    return letters = text.toLowerCase().replace(/ /g, "_");
+    return text.toLowerCase().replace(/ /g, "_");
 }
 let snakeCaseText = toSnakeCase(normalWords[0]);
-console.log(snakeCaseText)
-let allSnakeCaseText = normalWords.map(toSnakeCase);
-console.log(allSnakeCaseText, "snekk case")
+console.log(snakeCaseText, 'declared_snakeCase')
 
-//Arrow Function snake-case
-let snekkkCase = normalWords.map(text => {
-    return snekkLetters = text.toLowerCase().replace(/ /g, "_");
+// //Arrow Function snake-case
+let arrowSnake = normalWords.map(text => {
+    return text.toLowerCase().replace(/ /g, "_");
 })
-console.log(snekkkCase, 'all snekk arrow')
+console.log(arrowSnake, 'arrow_snake')
 
-//Declared Function kebab-case
+// //Declared Function kebab-case
 function toKebabCase(text){
- return kebabStick = text.toLowerCase().replace(/ /g, "-");
+ return text.toLowerCase().replace(/ /g, "-");
 }
 let kebabCaseText = toKebabCase(normalWords[0]);
-console.log(kebabCaseText, "keboob")
-let allKebabText = normalWords.map(toKebabCase);
-console.log(allKebabText, "all kebooob")
+console.log(kebabCaseText, "kebab-case")
 
-//Arrow Function kebab-case
-let kibob = normalWords.map(text => {
-    return kibobbStick = text.toLowerCase().replace(/ /g, "-");
+// //Arrow Function kebab-case
+let arrowKebabCase = normalWords.map(text => {
+    return text.toLowerCase().replace(/ /g, "-");
 })
-console.log(kibob, "arrow kebab case")
-// **************************************************
+console.log(arrowKebabCase, 'arrow-kebab-case')
+
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+// //   Create a function that will turn the name to classified data 
+// //   - get mr or mrs/ms to determine gender
+// //   - then transform data to object 
+ 
+// //   name: Andrew
+// //   gender: male
+// //   prefix: mr
+  
+// //   name: Kirstine
+// //   gender: female
+// //   prefix: ms
+  
+// //   name: Maria Ozawa
+// //   gender: female
+// //   prefix: mrs
+
+const names = [
+    "Mr Andrew",
+    "Ms Kirstine",
+    "Mr Yoso",
+    "Ms Taken",
+    "Mrs Maria Ozawa",
+    "Mr Some one bullshit"
+  ]
+
+//   //Declated Function
+function fullsetData(text){
+    let separateWord = text.split(" ");
+    let prefixOnly = separateWord.shift();
+    let fullName = separateWord.join(" ");
+    let gender 
+    if (text.includes('Mr')){
+        gender = 'male'
+    } else {
+        gender = 'female'
+    }
+
+    return {name: fullName,
+    gender: gender,
+    prefix: prefixOnly}
+
+}
+let wholeDataObj = names.map(fullsetData);
+console.log(wholeDataObj, 'declared-function')
+
+//Arrow Function
+let anotherVariable = names.map(text => {
+    let separateWord = text.split(" ");
+    let prefixOnly = separateWord.shift();
+    let fullName = separateWord.join(" ");
+    let gender 
+    if (text.includes('Mr')){
+        gender = 'male'
+    } else {
+        gender = 'female'
+    }
+
+    return {name: fullName,
+    gender: gender,
+    prefix: prefixOnly}
+})
+console.log(anotherVariable, 'arrow-function')
+
+
+// // **************************************************
+// //ANOTHER PROBLEM // 
+// // **************************************************
+
+const professions = [
+    "Dr Greg House",
+    "Prof. Albert Einstein",
+    "Pres. Barack Obama",
+    "Prof. Rick Sanchez",
+    "Pres. Bullshit Labrador",
+]
+
+// // Dr => Doctor
+// // Prof => Professor
+// // Pres => President
+
+
+// // Output should be
+
+// {
+//     first_name: Greg,
+//     last_name:  House,
+//     work: Doctor
+//     total_characters: 9 // count only name characters don't include prefix
+// }
+
+
+// //Declared Function with same variables/names
+
+function toGetProfession(text){
+    let completeNames = text.split(" ");
+    let firstName = completeNames[1];
+    let lastName = completeNames[2];
+    let totalChar = firstName.concat(lastName).length;
+    let work
+        if (text.includes('Prof.')) {
+            work =  "Professor"
+         } else if (text.includes('Pres.')) {
+            work = "President"
+         } else if (text.includes('Dr')) {
+            work = "Doctor"
+         };
+
+
+    return {
+        name: firstName,
+        last_name: lastName,
+        work: work,
+        totalChar: totalChar
+        };
+}
+let declaredF = toGetProfession(professions[0]);
+console.log(declaredF, 'declared function')
+
+let mappedName = professions.map(toGetProfession);
+console.log(mappedName, 'with map--declared')
+// //Arrow Function with same variables/names
+
+let varSample = professions.map(text => {
+    let completeNames = text.split(" ");
+    let firstName = completeNames[1];
+    let lastName = completeNames[2];
+    let totalChar = firstName.concat(lastName).length;
+    let work
+        if (text.includes('Prof.')) {
+            work =  "Professor"
+         } else if (text.includes('Pres.')) {
+            work = "President"
+         } else if (text.includes('Dr')) {
+            work = "Doctor"
+         };
+
+
+         return {
+             name: firstName,
+             last_name: lastName,
+             work: work,
+             totalChar: totalChar
+             };
+})
+
+console.log(varSample, 'arrow-function')
